93c1954b408a87e4871445fb2ea86c45e7e4cebf
CIC-SL\tania.chocan <tania.chocan@fieldeas.com>
2025-04-02 15:42:52 +0200

    añadido el metodo aleatorios

---
+++ ProyectoPrueba/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int NumeroAleatorio()
+{
+    Random rand = new Random();
+    return rand.Next(1, 101); // Número entre 1 y 100
+}
+
+
